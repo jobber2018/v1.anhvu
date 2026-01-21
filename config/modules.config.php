1ad7cf4eade4ae6f0d6b14c51c47fef911a973f4
jobber2018 <44490346+jobber2018@users.noreply.github.com>
2026-01-21 16:59:47 +0700

    first commit

---
+++ config/modules.config.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+/**
+ * List of enabled modules for this application.
+ *
+ * This should be an array of module namespaces used in the application.
+ */
+return [
+    'Zend\Mail',
+    'Zend\Navigation',
+    'Zend\Session',
+    'Zend\Db',
+    'Zend\Form',
+    'Zend\I18n',
+    'Zend\Mvc\I18n',
+    'Zend\Router',
+    'Zend\Validator',
+    'ZendDeveloperTools',
+    'DoctrineModule',
+    'DoctrineORMModule',
+    'Zend\Mvc\Plugin\FlashMessenger',
+    'Zf3\Geolocation',
+    'Application',
+    'Admin',
+    'Sulde',
+    'Users',
+    'Map',
+    'Grocery',
+    'GroceryCat',
+    'Product',
+    'Sell',
+    'Werehouse',
+    'Supplier',
+    'Report',
+    'Api',
+    'SellCheck'
+];
